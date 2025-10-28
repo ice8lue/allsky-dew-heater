816479bb7ffee76c91cad43a5512fc9739c9918f
Frank Adler <frank@adfr.io>
2025-10-28 14:42:05 +0100

    set correct script rights, remove install step

--- install.sh
+++ install.sh
@@ -4,10 +4,6 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install rpi.gpio
 
-chmod +x ./scripts/heater_ON.py ./scripts/heater_OFF.py fetchWeatherInfo.py
-chmod g+x ./scripts/heater_ON.py ./scripts/heater_OFF.py fetchWeatherInfo.py
-
-chmod g+w ./status.txt weather.txt
 chgrp www-data ./status.txt weather.txt
 
 sudo  cp  ./sudoers.txt  /etc/sudoers.d/dew_heater
