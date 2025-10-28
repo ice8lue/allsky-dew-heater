9d3b705c589ad90bb1c5a9bad8546636e1c37e55
Frank Adler <frank@adfr.io>
2025-10-28 14:44:01 +0100

    adjust dotenv package src

--- install.sh
+++ install.sh
@@ -2,10 +2,8 @@
 
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install rpi.gpio
+sudo apt-get install rpi.gpio python3-dotenv
 
 chgrp www-data ./status.txt weather.txt
 
 sudo  cp  ./sudoers.txt  /etc/sudoers.d/dew_heater
-
-pip install -r ./requirements.txt
