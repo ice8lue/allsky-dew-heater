a5de294fed67ae819fe5a524711d6b8a145018f1
Frank Adler <frank@adfr.io>
2025-10-28 14:38:46 +0100

    add fetchWeathrInfo script

--- install.sh
+++ install.sh
@@ -4,10 +4,12 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install rpi.gpio
 
-chmod +x ./scripts/heater_ON.py ./scripts/heater_OFF.py
-chmod g+x ./scripts/heater_ON.py ./scripts/heater_OFF.py
+chmod +x ./scripts/heater_ON.py ./scripts/heater_OFF.py fetchWeatherInfo.py
+chmod g+x ./scripts/heater_ON.py ./scripts/heater_OFF.py fetchWeatherInfo.py
 
-chmod g+w ./status.txt
-chgrp www-data ./status.txt
+chmod g+w ./status.txt weather.txt
+chgrp www-data ./status.txt weather.txt
 
 sudo  cp  ./sudoers.txt  /etc/sudoers.d/dew_heater
+
+pip install -r ./requirements.txt
---
+++ scripts/fetchWeatherInfo.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import urllib.request
+import json
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+apiUrl = f"https://api.weatherapi.com/v1/current.json?key={os.getenv('WEATHER_API_KEY')}&q={os.getenv('LOCATION')}&aqi=no"
+
+with urllib.request.urlopen(apiUrl) as url:
+    data = json.loads(url.read().decode())
+
+temp = data["current"]["temp_c"]
+dewpoint = data["current"]["dewpoint_c"]
+
+# Write output file
+tab = "\t"
+newline = "\n"
+weatherFile = "/home/adfr/allsky-dew-heater/weather.txt"
+
+with open(weatherFile, "w") as outfile:
+    outfile.write("data")
+    outfile.write(tab)
+    outfile.write("0")
+    outfile.write(tab)
+    outfile.write("Temperature Outside")
+    outfile.write(tab)
+    outfile.write(temp)
+    outfile.write(newline)
+
+    outfile.write("data")
+    outfile.write(tab)
+    outfile.write("0")
+    outfile.write(tab)
+    outfile.write("Dew Point")
+    outfile.write(tab)
+    outfile.write(dewpoint)
