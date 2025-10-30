1b1731c758d6cbbf2c1cd45b41976b25db2bfe33
Frank Adler <frank@adfr.io>
2025-10-30 22:43:06 +0100

    adjust image upload

--- scripts/files.py
+++ scripts/files.py
@@ -1,5 +1,6 @@
 weatherFile = "/home/adfr/allsky-dew-heater/weather.txt"
 statusFile = "/home/adfr/allsky-dew-heater/status.txt"
+latestImageFile = "/home/adfr/tmp/image-*.jpg"
 
 # Helpers
 tab = "\t"
--- scripts/uploadLatest.py
+++ scripts/uploadLatest.py
@@ -2,18 +2,23 @@
 
 import os
 import requests
+import glob
 from dotenv import load_dotenv
+from files import latestImageFile
 
 load_dotenv()
 
 url = 'https://api.adfr.io/files/61badcf5-3a08-4ecb-9ca2-cebf4cdd5288'
 
-with open('/home/adfr/tmp/latest.jpg', 'rb') as img:
-    name_img= os.path.basename('/home/adfr/tmp/latest.jpg')
-    files= {'image': (name_img,img,'multipart/form-data',{'Expires': '0'}) }
+images = glob.glob(latestImageFile)
+for imageFile in images:
+    with open(imageFile, 'rb') as img:
+        name_img= os.path.basename(imageFile)
+        files= {'image': (name_img,img,'multipart/form-data',{'Expires': '0'}) }
 
-    requests.patch(
-        url, 
-        files=files,
-        headers={'Authorization': f'Bearer {os.getenv("DIRECTUS_API_KEY")}'}
-    )
+        requests.patch(
+            url, 
+            files=files,
+            headers={'Authorization': f'Bearer {os.getenv("DIRECTUS_API_KEY")}'}
+        )
+    os.remove(imageFile)
