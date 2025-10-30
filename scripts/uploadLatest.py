fb4903ff7840c9556f9388bdbe6451ba8ca14a0b
Frank Adler <frank@adfr.io>
2025-10-30 23:21:56 +0100

    add docs

--- scripts/uploadLatest.py
+++ scripts/uploadLatest.py
@@ -8,23 +8,31 @@ from files import latestImageFile
 
 load_dotenv()
 
+# Directus file entry URL to update
 url = 'https://api.adfr.io/files/61badcf5-3a08-4ecb-9ca2-cebf4cdd5288'
 
 
-
+# get all images matching the pattern
 images = glob.glob(latestImageFile)
+
+# in case there are multiple, upload them all
 for imageFile in images:
     with open(imageFile, 'rb') as img:
         filename = os.path.basename(imageFile)
+
+        # read image data
         imageData = img.read()
 
         files = {
             'file': (filename, imageData, 'image/jpeg')
         }
 
+        # make the PATCH request to update the image entry
         requests.patch(
             url, 
             files=files,
             headers={'Authorization': f'Bearer {os.getenv("DIRECTUS_API_KEY")}'}
         )
+    
+    # remove the image file after upload
     os.remove(imageFile)
