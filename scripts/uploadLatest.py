10cb3707f164dc4c5270c9143808b40ad8ba6d79
Frank Adler <frank@adfr.io>
2025-10-30 23:04:38 +0100

    add content type

--- scripts/uploadLatest.py
+++ scripts/uploadLatest.py
@@ -10,11 +10,13 @@ load_dotenv()
 
 url = 'https://api.adfr.io/files/61badcf5-3a08-4ecb-9ca2-cebf4cdd5288'
 
+
+
 images = glob.glob(latestImageFile)
 for imageFile in images:
     with open(imageFile, 'rb') as img:
         name_img= os.path.basename(imageFile)
-        files= {'file': (name_img, img, 'multipart/form-data', {'Expires': '0'})}
+        files= {'file': ( name_img, img, 'Content-Type': 'image/jpeg', 'Content-Length': 1 )}
 
         requests.patch(
             url, 
