0034a20c8bac2df41262c4e8e25116e17ac3ba34
Frank Adler <frank@adfr.io>
2025-10-30 22:34:50 +0100

    adjust image loading

--- scripts/uploadLatest.py
+++ scripts/uploadLatest.py
@@ -8,10 +8,12 @@ load_dotenv()
 
 url = 'https://api.adfr.io/files/61badcf5-3a08-4ecb-9ca2-cebf4cdd5288'
 
-files = {'media': open('/home/adfr/tmp/latest.jpg', 'rb')}
+with open('/home/adfr/tmp/latest.jpg', 'rb') as img:
+    name_img= os.path.basename('/home/adfr/tmp/latest.jpg')
+    files= {'image': (name_img,img,'multipart/form-data',{'Expires': '0'}) }
 
-requests.patch(
-    url, 
-    files=files,
-    headers={'Authorization': f'Bearer {os.getenv("DIRECTUS_API_KEY")}'}
-)
+    requests.patch(
+        url, 
+        files=files,
+        headers={'Authorization': f'Bearer {os.getenv("DIRECTUS_API_KEY")}'}
+    )
