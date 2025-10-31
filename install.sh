e6860fed8d1af28f4819e1243f7f48763eb66758
Frank Adler <frank@adfr.io>
2025-10-31 23:15:35 +0100

    update install script

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install rpi.gpio python3-dotenv python3-requests
 
-chgrp www-data ./status.txt weather.txt
+chgrp www-data status.txt weather.txt heater-log.txt heater-last-active.txt
 
 sudo  cp  ./sudoers.txt  /etc/sudoers.d/dew_heater
